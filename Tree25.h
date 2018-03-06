66e060d8046a208a165f9d94c8555a9ed3e88069
lawrencekhlim <lawrencekhlim@gmail.com>
2018-03-05 21:39:18 -0800

    Update to header file of 2-5 Tree

--- Tree25.h
+++ Tree25.h
@@ -1,29 +1,30 @@
-#ifndef AVL_H
-#define AVL_H
-#include "node.h"
+#ifndef TREE25_H
+#define TREE25_H
+#include "node25.h"
 #include <vector>
 
-class AVL
+class Tree25
 {
 	private:
-		node* head;
-        void insert(string str, node* head);
-        int search(string str, node* head);
-        node* searchNode(string str, node* head);
-        void rangeSearches (string a, string b, node* pointer, vector <string> &list);
-        node* deleteNodeHelper (string str, node* pointer);
+		node25* head;
+        void insert(string str, node25* head);
+        int search(string str, node25* head);
+        node25* searchNode(string str, node25* head);
+        void rangeSearches (string a, string b, node25* pointer, vector <string> &list);
+        node25* deleteNodeHelper (string str, node25* pointer);
 	public:
-		AVL();
-        ~AVL();
+		Tree25();
+        ~Tree25();
+    
 		void insert(string str);
-		void printInOrder(node* headz);
-        void printTree (node* headz, string spaces);
-		int countInTree(node* head);
+		void printInOrder(node25* headz);
+        void printTree (node25* headz, string spaces);
+		int countInTree(node25* head);
 		int search(string str);
 		void readInTree(string a[],int length);
 		void deleteNode(string str);
-		node* getHead();
+		node25* getHead();
         vector <string>rangeSearch (string a, string b);
-        void sorted (node* headz, vector <string> &list);
+        void sorted (node25* headz, vector <string> &list);
 };
 #endif

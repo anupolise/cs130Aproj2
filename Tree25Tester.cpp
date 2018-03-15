8292ba0690b532cdd362b6cd6af9589e63f98e48
lawrencekhlim <lawrencekhlim@gmail.com>
2018-03-14 23:50:20 -0700

    Completed search, bug fixes to delete

--- Tree25Tester.cpp
+++ Tree25Tester.cpp
@@ -6,16 +6,17 @@ using namespace std;
 
 int main()
 {
-    string arr[40] = {"aa","ja","ja", "bd", "bd", "ja", "ra", "bb", "zz", "ff", "df", "gg", "gg", "dd", "ds", "hi", "ai", "at", "as", "av", "hh", "hb", "bc", "cf", "gh", "he", "ha", "zg", "sd", "sc", "zg", "gk", "zh", "ss", "sd", "sh", "sf", "sj"};
+    string arr[100] = {
+        "aa", "ja","ja", "bd", "bd", "ja", "ra", "bb", "zz", "ff", "df", "gg", "gg", "dd", "ds", "hi", "ai", "at", "as", "av", "hh", "hb", "bc", "cf", "gh", "he", "ha", "zg", "sd", "sc", "zg", "gk", "zh", "ss", "sd", "sh", "sf", "sj", "gh", "bb", "hj", "uc", "vd", "qw", "wa", "po", "li", "ju", "rf", "rb", "bb", "xi", "os", "dk", "lt", "ef", "qa", "qj", "bt", "zo", "nf"};
     Tree25 tree;
-    tree.readInTree(arr,37);
+    tree.readInTree(arr,51);
     
     cout<<"header: "<<(tree.getHead())->getData(0)<<endl;
     
     tree.printTree(tree.getHead(), "");
     
     string arr2[40] = {"sh","ss","sd", "sf", "ra", "ja", "ra", "bb", "zz", "ff", "df", "gg", "gg", "dd", "ds", "hi", "ai", "at", "as", "av", "hh", "hb", "bc", "cf", "gh", "he", "ha", "zg", "sd", "sc", "zg", "gk", "zh", "ss", "sd", "sh", "sf", "sj"};
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 0; i++) {
         cout << "Delete " << arr2[i]<< endl;
         tree.deleteNode (arr2[i]);
         tree.printTree(tree.getHead(), "");
